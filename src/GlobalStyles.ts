63fd1a3325c4eb4b70ee40e78ee44f1954bb9cbc
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-12 13:19:09 +0100

    updated GlobalStyles

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,18 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
-html, body {
+  html, body {
     background-color: #212121;
     color: white;
     font-family: 'Roboto', sans-serif;
   }
 
-h1, h2, h3, h4, h5, h6, p {
-  cursor: default;
-}
+  h1, h2, h3, h4, h5, h6, p {
+    cursor: default;
+  }
+
+  #fp-nav ul li a span {
+    background: white;
+  }
+
+  #fp-nav ul li .active span {
+    background: green;
+  }
 `;
