1110f01437e52c566e2785e76074f16a60266a16
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-12 12:50:11 +0100

    Navbar setup

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components/macro';
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Logo>ŽIGA HAJŠEN</Logo>
+    </Container>
+  );
 };
 
 export default Navbar;
+
+const Container = styled.div`
+  position: absolute;
+  z-index: 1;
+  width: 100vw;
+  height: 10%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  margin-left: 50px;
+`;
