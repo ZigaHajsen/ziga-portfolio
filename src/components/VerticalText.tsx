764b3ed5893ee7e691a8d967fda917d08417a2af
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-14 00:22:29 +0100

    added VerticalText component

---
+++ src/components/VerticalText.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const VerticalText: React.FC = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default VerticalText;
+
+export const Container = styled.p`
+  writing-mode: vertical-lr;
+  transform: rotate(180deg);
+  position: fixed;
+  left: 0px;
+  font-size: 150px;
+  height: 100vh;
+  width: 200px;
+  margin-block-end: 0;
+  margin-block-start: 0;
+  display: flex;
+  justify-content: center;
+  opacity: 0.1;
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Background } from './Background';
 export { default as Card } from './Card';
+export { default as VerticalText } from './VerticalText';
