084cb458a8893280b060be8bcd4ff5dd2cf5f8cf
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-18 20:09:33 +0100

    added temporarl work data

---
+++ src/data/work.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+export const work = [
+  {
+    id: 1,
+    image: '../../public/images/projects/1.png',
+    title: 'Portfolio Website',
+    subTitle: 'Frontend React App',
+    github: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+    liveSite: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+  },
+  {
+    id: 2,
+    image: '../../public/images/projects/1.png',
+    title: 'Portfolio Website',
+    subTitle: 'Frontend React App',
+    github: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+    liveSite: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+  },
+  {
+    id: 3,
+    image: '../../public/images/projects/1.png',
+    title: 'Portfolio Website',
+    subTitle: 'Frontend React App',
+    github: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+    liveSite: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+  },
+  {
+    id: 4,
+    image: '../../public/images/projects/1.png',
+    title: 'Portfolio Website',
+    subTitle: 'Frontend React App',
+    github: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+    liveSite: {
+      url: 'https://github.com/ZigaHajsen',
+      target: '_blank',
+      icon: <FaGithub />,
+    },
+  },
+];
